db35701a7027d389780993abd7f8738e66a3c388
kompasim <kompasim@163.com>
2021-06-25 02:18:33 +0800

    update timer

--- files/timer.lua
+++ files/timer.lua
@@ -3,71 +3,48 @@
 ]]
 local timer = {}
 local timers = {}
-local pool = setmetatable({}, {
-    __mode = "kv"
-})
 
-local function timer_insert(sec, fn)
-    local expiretime = os.clock() + sec
+local function timer_insert(sec, action)
+    local deadline = os.clock() + sec
     local pos = 1
     for i, v in ipairs(timers) do
-        if v.expiretime > expiretime then
+        if v.deadline > deadline then
             break
         end
         pos = i + 1
     end
-    local context = {
-        expiretime = expiretime,
-        fn = fn
+    local tm = {
+        deadline = deadline,
+        action = action
     }
-    table.insert(timers, pos, context)
-    return context
+    table.insert(timers, pos, tm)
 end
 
-local function timer_resume(co, ...)
-    local ok, err = coroutine.resume(co, ...)
-    if not ok then
-        error(debug.traceback(co, err))
-    end
-    return ok, err
-end
-
-local function timer_body(fn)
-    local co = coroutine.running()
-    while true do
-        fn()
-        pool[#pool + 1] = co
-        fn = coroutine.yield()
+local function timer_check()
+    local tm = timers[1]
+    if tm.deadline <= os.clock() then
+        table.remove(timers, 1)
+        local isOk, error = xpcall(tm.action, debug.traceback)
+        if isOk then return end
+        print(error)
     end
 end
 
-local function timer_start()
-    while #timers > 0 do
-        local tm = timers[1]
-        if tm.expiretime <= os.clock() then
-            table.remove(timers, 1)
-            if not tm.remove then
-                local ok, err = xpcall(tm.fn, debug.traceback)
-                if not ok then
-                    print("timer error:", err)
-                end
-            end
-        else
-            break
-        end
-    end
+function timer.flag()
+    return {}
 end
 
-function timer.async(fn)
-    local co = table.remove(pool)
-    if not co then
-        co = coroutine.create(timer_body)
-    end
-    local _, res = timer_resume(co, fn)
-    if res then
-        return res
-    end
-    return co
+function timer.finish(flag)
+    flag.ok = true
+end
+
+function timer.running(flag)
+    return flag.ok ~= true
+end
+
+function timer.async(func)
+    local co = coroutine.create(func)
+    coroutine.resume(co)
 end
 
 function timer.sleep(seconds)
@@ -75,29 +52,38 @@ function timer.sleep(seconds)
     timer_insert(seconds, function()
         coroutine.resume(co)
     end)
-    return coroutine.yield()
+    coroutine.yield()
 end
 
-function timer.delay(seconds, fn)
-    return timer_insert(seconds, fn)
+function timer.wait(flag)
+    while timer.running(flag) do
+        timer.sleep(0.1)
+    end
 end
 
-function timer.circle(seconds, fn)
-    return timer.delay(seconds, function()
-        local ctx = timer.circle(seconds, fn)
-        fn(ctx)
-        timer.start()
+function timer.delay(seconds, func)
+    local flag = timer.flag()
+    timer_insert(seconds, function()
+        if not timer.running(flag) then
+            return
+        end
+        local isOk, s = xpcall(func, debug.traceback)
+        if not isOk then
+            print(s)
+            return
+        end
+        if not s or s <= 0 then
+            timer.finish(flag)
+        else
+            timer.delay(s, func)
+        end
     end)
-end
-
-function timer.cancel(ctx)
-    assert(type(ctx) == 'table' and ctx.fn ~= nil)
-    ctx.remove = true
+    return flag
 end
 
 function timer.start()
     while #timers > 0 do
-        timer_start()
+        timer_check()
     end
 end
 
