5046fb847518fbe2c40d206163b804a0c0b5e92e
kompasim <kompasim@163.com>
2022-01-26 01:45:48 +0800

    update

--- test.lua
+++ test.lua
@@ -6,14 +6,21 @@ local f = debug.getinfo(function() end)['short_src']
 local d = string.gsub(f, "%a+%.%a+", "")
 package.path = package.path .. ";" .. d .. "/?.lua;"
 
-local MY_WORK_DIR = "./" -- os.getenv("HOME")
-assert(MY_WORK_DIR ~= nil, "[PURE_LUA_TOOLS] please set [HOMVE] env variable !")
-package.path = package.path .. ";" .. MY_WORK_DIR .. "/.pure-lua-tools/?.lua"
-xpcall(require, function() end,"initialize")
-if not class then
-    print('[PURE_LUA_TOOLS] downloading ...')
-    os.execute("git clone git@github.com:kompasim/pure-lua-tools.git " .. MY_WORK_DIR .. "/.pure-lua-tools")
-    xpcall(require, function() end, "initialize")
-    assert(class ~= nil, "[PURE_LUA_TOOLS] download failed!")
-    print('[PURE_LUA_TOOLS] download succeed!')
+local function download_and_import_by_git(gitUrl, entryName, workingDir)
+    local slashPos = string.find(string.reverse(gitUrl), "/", 1, true)
+    local pointPos = string.find(string.reverse(gitUrl), ".", 1, true)
+    assert(slashPos ~= nil and pointPos ~= nil and slashPos > pointPos, "[LUA_GIT_IMPORT] invalid url:" .. gitUrl)
+    local folderName = "." .. string.sub(gitUrl, #gitUrl - slashPos + 2, #gitUrl - pointPos) .. "/"
+    workingDir = workingDir or os.getenv("HOME")
+    assert(workingDir ~= nil, "[LUA_GIT_IMPORT] working dir not found !")
+    package.path = package.path .. ";" .. workingDir .. "/" .. folderName .. "?.lua"
+    local isOk, err = pcall(require, entryName)
+    if not isOk then
+        print('[LUA_GIT_IMPORT] downloading ...')
+        os.execute("git clone " .. gitUrl .. " " .. workingDir .. "/" .. folderName)
+        isOk, err = pcall(require, entryName)
+        assert(isOk, "[LUA_GIT_IMPORT] import failed:" .. err)
+        print('[LUA_GIT_IMPORT] import succeeded!')
+    end
 end
+download_and_import_by_git("git@github.com:kompasim/pure-lua-tools.git", "initialize", "./")
