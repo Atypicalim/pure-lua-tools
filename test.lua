3bb71f88ed54e8fc08bf0124433b0e2e0677705a
kompasim <kompasim@163.com>
2022-01-25 12:43:55 +0800

    install

--- test.lua
+++ test.lua
@@ -6,4 +6,11 @@ local f = debug.getinfo(function() end)['short_src']
 local d = string.gsub(f, "%a+%.%a+", "")
 package.path = package.path .. ";" .. d .. "/?.lua;"
 
-require("initialize")
+xpcall(require, function() end, "pure-lua-tools.initialize")
+if not class then
+    print('[PURE_LUA_TOOLS] downloading ...')
+    os.execute('git clone git@github.com:kompasim/pure-lua-tools.git')
+    xpcall(require, function() end, "pure-lua-tools.initialize")
+    assert(class ~= nil, '[PURE_LUA_TOOLS] download failed!')
+    print('[PURE_LUA_TOOLS] download succeed!')
+end
