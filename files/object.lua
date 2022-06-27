29b8e38f77376da6e9c14692685bd7e4bac1954b
kompasim <kompasim@163.com>
2022-06-27 22:46:02 +0800

    update object

--- files/object.lua
+++ files/object.lua
@@ -25,21 +25,13 @@ function Object:ext()
 end
 
 function Object:is(Cls)
-    assert(self.__class == nil, 'can not check Object!')
+    assert(self.__class ~= nil, 'can not check Object!')
     local mt = getmetatable(self)
     while mt do
         if mt == Cls then return true end
         mt = getmetatable(mt)
     end
     return false
-end
-
-function Object:__tostring()
-    local tmp = getmetatable(self)
-    setmetatable(self, nil)
-    local info = tostring(self)
-    setmetatable(self, tmp)
-    return string.format("<%s_%s>", self.__class and "object" or "Object", info) 
 end
 
 return Object
--- test.lua
+++ test.lua
@@ -2,25 +2,43 @@
     test
 ]]
 
-local f = debug.getinfo(function() end)['short_src']
-local d = string.gsub(f, "%a+%.%a+", "")
-package.path = package.path .. ";" .. d .. "/?.lua;"
+-- local f = debug.getinfo(function() end)['short_src']
+-- local d = string.gsub(f, "%a+%.%a+", "")
+-- package.path = package.path .. ";" .. d .. "/?.lua;"
 
-local function download_and_import_by_git(gitUrl, entryName, workingDir)
-    local slashPos = string.find(string.reverse(gitUrl), "/", 1, true)
-    local pointPos = string.find(string.reverse(gitUrl), ".", 1, true)
-    assert(slashPos ~= nil and pointPos ~= nil and slashPos > pointPos, "[LUA_GIT_IMPORT] invalid url:" .. gitUrl)
-    local folderName = "." .. string.sub(gitUrl, #gitUrl - slashPos + 2, #gitUrl - pointPos) .. "/"
-    workingDir = workingDir or os.getenv("HOME")
-    assert(workingDir ~= nil, "[LUA_GIT_IMPORT] working dir not found !")
-    package.path = package.path .. ";" .. workingDir .. "/" .. folderName .. "?.lua"
-    local isOk, err = pcall(require, entryName)
-    if not isOk then
-        print('[LUA_GIT_IMPORT] downloading ...')
-        os.execute("git clone " .. gitUrl .. " " .. workingDir .. "/" .. folderName)
-        isOk, err = pcall(require, entryName)
-        assert(isOk, "[LUA_GIT_IMPORT] import failed:" .. err)
-        print('[LUA_GIT_IMPORT] import succeeded!')
-    end
+-- local function download_and_import_by_git(gitUrl, entryName, workingDir)
+--     local slashPos = string.find(string.reverse(gitUrl), "/", 1, true)
+--     local pointPos = string.find(string.reverse(gitUrl), ".", 1, true)
+--     assert(slashPos ~= nil and pointPos ~= nil and slashPos > pointPos, "[LUA_GIT_IMPORT] invalid url:" .. gitUrl)
+--     local folderName = "." .. string.sub(gitUrl, #gitUrl - slashPos + 2, #gitUrl - pointPos) .. "/"
+--     workingDir = workingDir or os.getenv("HOME")
+--     assert(workingDir ~= nil, "[LUA_GIT_IMPORT] working dir not found !")
+--     package.path = package.path .. ";" .. workingDir .. "/" .. folderName .. "?.lua"
+--     local isOk, err = pcall(require, entryName)
+--     if not isOk then
+--         print('[LUA_GIT_IMPORT] downloading ...')
+--         os.execute("git clone " .. gitUrl .. " " .. workingDir .. "/" .. folderName)
+--         isOk, err = pcall(require, entryName)
+--         assert(isOk, "[LUA_GIT_IMPORT] import failed:" .. err)
+--         print('[LUA_GIT_IMPORT] import succeeded!')
+--     end
+-- end
+-- download_and_import_by_git("git@github.com:kompasim/pure-lua-tools.git", "initialize", "./")
+
+require 'initialize'
+
+local Human = Object:ext()
+function Human:init()
+    print('human...')
 end
-download_and_import_by_git("git@github.com:kompasim/pure-lua-tools.git", "initialize", "./")
+-- extended class
+local Man = Human:ext()
+function Man:init()
+    Human.init(self)
+    print('man...')
+end
+-- a object
+local alex = Man:new()
+print(alex:is(Man))
+print(alex:is(Human))
+
