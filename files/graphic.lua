70302ca5f82e1b6fc73741463c97b58f666164f0
kompasim <kompasim@163.com>
2022-11-23 14:33:56 +0800

    upadte

--- files/graphic.lua
+++ files/graphic.lua
@@ -21,6 +21,10 @@ local COMMON_HEADER = [[
 [reflection.assembly]::LoadWithPartialName( "System.Drawing");
 $brush = new-object Drawing.SolidBrush "#22ffcc"
 $pen = new-object Drawing.Pen "#22ffcc"
+$x = 0
+$y = 0
+$w = 250
+$h = 250
 ]]
 
 local FORM_CREATE = [[
@@ -53,79 +57,82 @@ $bitmap.Save("%s")
 $graphics.Dispose()
 ]]
 
-local DIALOG_TEXT = [[
+local GRAPHIC_STYLE = [[
 $brush.color = "%s"
+$pen.color = "%s"
+$pen.width = %d
+]]
+
+local GRAPHIC_POSITION = [[
+$x = %d
+$y = %d
+]]
+
+local GRAPHIC_SIZE = [[
+$w = %d
+$h = %d
+]]
+
+local DIALOG_TEXT = [[
 $font = new-object System.Drawing.Font "%s",%d
-$graphics.DrawString('%s', $font, $brush, %d, %d);
+$graphics.DrawString('%s', $font, $brush, $x, $y);
 ]]
 
 local DIALOG_IMAGE = [[
 $file = (get-item '%s')
 $img = [System.Drawing.Image]::Fromfile($file);
 $units = [System.Drawing.GraphicsUnit]::Pixel
-$dest = new-object Drawing.Rectangle %d, %d, %d, %d
+$dest = new-object Drawing.Rectangle $x, $y, 200, 200
 $src = new-object Drawing.Rectangle %d, %d, %d, %d
 $graphics.DrawImage($img, $dest, $src, $units);
 ]]
 
 local DIALOG_ELLIPSE = [[
-$brush.color = "%s"
-$rect = new-object Drawing.Rectangle %d, %d, %d, %d
+$rect = new-object Drawing.Rectangle $x, $y, $w, $h
 $graphics.FillEllipse($brush, $rect);
 ]]
 
 local DIALOG_RECTANGLE = [[
-$brush.color = "%s"
-$rect = new-object Drawing.Rectangle %d, %d, %d, %d
+$rect = new-object Drawing.Rectangle $x, $y, $w, $h
 $graphics.FillRectangle($brush, $rect);
 ]]
 
-local DIALOG_BEZIER = [[
-$pen.color = "%s"
-$pen.width = %d
-$p = new-object Drawing.Point %d, %d;
-%s
-$points = $p, %s
-$graphics.DrawBeziers($pen, $points);
-]]
-
 local DIALOG_LINE = [[
-$pen.color = "%s"
-$pen.width = %d
 %s
 $points = %s
 $graphics.DrawLines($pen, $points);
 ]]
 
 local DIALOG_CURVE = [[
-$pen.color = "%s"
-$pen.width = %d
 %s
 $points = %s
 $graphics.DrawCurve($pen, $points);
 ]]
 
+local DIALOG_BEZIER = [[
+%s
+$points = %s
+$graphics.DrawBeziers($pen, $points);
+]]
+
 local DIALOG_PIE = [[
-$pen.color = "%s"
-$pen.width = %d
-$graphics.DrawPie($pen, %d, %d, %d, %d, %d, %d);
+$graphics.DrawPie($pen, $x, $y, $w, $h, %d, %d);
 ]]
 
 local DIALOG_ARC = [[
-$pen.color = "%s"
-$pen.width = %d
-$graphics.DrawArc($pen, %d, %d, %d, %d, %d, %d);
+$graphics.DrawArc($pen, $x, $y, $w, $h, %d, %d);
 ]]
 
 local DIALOG_POLYGON = [[
-$pen.color = "%s"
-$pen.width = %d
 %s
 $points = %s
 $graphics.DrawPolygon($pen, $points);
 ]]
 
 local TYPES = {
+    STYLE = "STYLE",
+    POSITION = "POSITION",
+    SIZE = "SIZE",
     TEXT = "TEXT",
     ELLIPSE = "ELLIPSE",
     RECTANGLE = "RECTANGLE",
@@ -141,54 +148,12 @@ function Node:__init__(tp, map)
     self.tp = tp
     self.x = 0
     self.y = 0
-    self.w = 100
-    self.h = 25
-    self.length = 100
-    self.color = "#eeeeee"
-    self.text = "Unknown..."
-    self.font = "Microsoft Sans Serif"
-    self.size = 13
+    self.w = 250
+    self.h = 250
     self.ext = nil
     for k,v in pairs(map or {}) do
         self[k] = v
     end
-end
-
-function Node:setXY(x, y)
-    self.x = x or self.x
-    self.y = y or self.y
-    return self
-end
-
-function Node:setWH(w, h)
-    self.w = w or self.w
-    self.h = h or self.h
-    return self
-end
-
-function Node:setLength(length)
-    self.length = lengthh
-    return self
-end
-
-function Node:setColor(color)
-    self.color = color
-    return self
-end
-
-function Node:setText(text)
-    self.text = text
-    return self
-end
-
-function Node:setFont(font)
-    self.font = font
-    return self
-end
-
-function Node:setSize(size)
-    self.size = size
-    return self
 end
 
 function Node:setExt(ext)
@@ -203,9 +168,32 @@ function Graphic:__init__(w, h)
     self._code = ""
 end
 
-function Graphic:addText(text)
+function Graphic:setStyle(color, size)
+    color = color or "#eeeeee"
+    size = size or 10
+    table.insert(self._children, Node(TYPES.STYLE):setExt({color, size}))
+    return self
+end
+
+function Graphic:setXY(x, y)
+    x = x or 0
+    y = y or 0
+    table.insert(self._children, Node(TYPES.POSITION):setExt({x, y}))
+    return self
+end
+
+function Graphic:setWH(w, h)
+    w = w or 0
+    h = h or 0
+    table.insert(self._children, Node(TYPES.SIZE):setExt({w, h}))
+    return self
+end
+
+function Graphic:addText(text, size, font)
     text = text or "Text..."
-    table.insert(self._children, Node(TYPES.TEXT):setXY(0, 0):setText(text))
+    size = size or 13
+    font = font or "Microsoft Sans Serif"
+    table.insert(self._children, Node(TYPES.TEXT):setExt({text, size, font}))
     return self._children[#self._children]
 end
 
@@ -215,24 +203,17 @@ function Graphic:addImage(path, fromX, fromY, fromW, fromH)
     fromY = fromY or 0
     fromW = fromW or 250
     fromH = fromH or 250
-    table.insert(self._children, Node(TYPES.IMAGE):setWH(200, 200):setExt({path, fromX, fromY, fromW, fromH}))
+    table.insert(self._children, Node(TYPES.IMAGE):setExt({path, fromX, fromY, fromW, fromH}))
     return self._children[#self._children]
 end
 
 function Graphic:addEllipse()
-    table.insert(self._children, Node(TYPES.ELLIPSE):setWH(100, 100))
+    table.insert(self._children, Node(TYPES.ELLIPSE))
     return self._children[#self._children]
 end
 
 function Graphic:addRectangle()
-    table.insert(self._children, Node(TYPES.RECTANGLE):setWH(100, 100))
-    return self._children[#self._children]
-end
-
-function Graphic:addBezier(cPointA1, cPointB1, end1, ...)
-    local points = {cPointA1, cPointB1, end1, ...}
-    points = #points > 0 and points or {{100, 100}, {200, 10}, {200, 200}}
-    table.insert(self._children, Node(TYPES.BEZIER):setXY(0, 0):setExt(points))
+    table.insert(self._children, Node(TYPES.RECTANGLE))
     return self._children[#self._children]
 end
 
@@ -250,13 +231,20 @@ function Graphic:addCurve(point1, point2, ...)
     return self._children[#self._children]
 end
 
+function Graphic:addBezier(start, cPointA1, cPointB1, end1, ...)
+    local points = {start, cPointA1, cPointB1, end1, ...}
+    points = #points > 0 and points or {{10, 10}, {100, 100}, {200, 10}, {200, 200}}
+    table.insert(self._children, Node(TYPES.BEZIER):setExt(points))
+    return self._children[#self._children]
+end
+
 function Graphic:addPie(fromR, toR)
-    table.insert(self._children, Node(TYPES.PIE):setWH(100, 100):setExt({fromR or 0, toR or 270}))
+    table.insert(self._children, Node(TYPES.PIE):setExt({fromR or 0, toR or 270}))
     return self._children[#self._children]
 end
 
 function Graphic:addArc(fromR, toR)
-    table.insert(self._children, Node(TYPES.ARC):setWH(100, 100):setExt({fromR or 0, toR or 270}))
+    table.insert(self._children, Node(TYPES.ARC):setExt({fromR or 0, toR or 270}))
     return self._children[#self._children]
 end
 
@@ -277,30 +265,36 @@ function Graphic:_formatPoints(points)
 end
 
 function Graphic:_processChild(i, v)
-    if v.tp == TYPES.TEXT then
-        self._code = self._code .. string.format(DIALOG_TEXT, v.color, v.font, v.size, v.text, v.x, v.y)
+    if v.tp == TYPES.STYLE then
+        self._code = self._code .. string.format(GRAPHIC_STYLE, v.ext[1], v.ext[1], v.ext[2])
+    elseif v.tp == TYPES.POSITION then
+        self._code = self._code .. string.format(GRAPHIC_POSITION, v.ext[1], v.ext[2])
+    elseif v.tp == TYPES.SIZE then
+        self._code = self._code .. string.format(GRAPHIC_SIZE, v.ext[1], v.ext[2])
+    elseif v.tp == TYPES.TEXT then
+        self._code = self._code .. string.format(DIALOG_TEXT, v.ext[3], v.ext[2], v.ext[1])
     elseif v.tp == TYPES.IMAGE then
-        self._code = self._code .. string.format(DIALOG_IMAGE, v.ext[1], v.x, v.y, v.w, v.h, v.ext[2], v.ext[3], v.ext[4], v.ext[5])
+        self._code = self._code .. string.format(DIALOG_IMAGE, v.ext[1], v.ext[2], v.ext[3], v.ext[4], v.ext[5])
     elseif v.tp == TYPES.ELLIPSE then
-        self._code = self._code .. string.format(DIALOG_ELLIPSE, v.color, v.x, v.y, v.w, v.h)
+        self._code = self._code .. string.format(DIALOG_ELLIPSE)
     elseif v.tp == TYPES.RECTANGLE then
-        self._code = self._code .. string.format(DIALOG_RECTANGLE, v.color, v.x, v.y, v.w, v.h)
-    elseif v.tp == TYPES.BEZIER then
-        local names, bodies = self:_formatPoints(v.ext)
-        self._code = self._code .. string.format(DIALOG_BEZIER, v.color, v.size, v.x, v.y, bodies, names)
+        self._code = self._code .. string.format(DIALOG_RECTANGLE)
     elseif v.tp == TYPES.LINE then
         local names, bodies = self:_formatPoints(v.ext)
-        self._code = self._code .. string.format(DIALOG_LINE, v.color, v.size, bodies, names)
+        self._code = self._code .. string.format(DIALOG_LINE, bodies, names)
     elseif v.tp == TYPES.CURVE then
         local names, bodies = self:_formatPoints(v.ext)
-        self._code = self._code .. string.format(DIALOG_CURVE, v.color, v.size, bodies, names)
+        self._code = self._code .. string.format(DIALOG_CURVE, bodies, names)
+    elseif v.tp == TYPES.BEZIER then
+        local names, bodies = self:_formatPoints(v.ext)
+        self._code = self._code .. string.format(DIALOG_BEZIER, bodies, names)
     elseif v.tp == TYPES.PIE then
-        self._code = self._code .. string.format(DIALOG_PIE, v.color, v.size, v.x, v.y, v.w, v.h, v.ext[1], v.ext[2])
+        self._code = self._code .. string.format(DIALOG_PIE, v.ext[1], v.ext[2])
     elseif v.tp == TYPES.ARC then
-        self._code = self._code .. string.format(DIALOG_ARC, v.color, v.size, v.x, v.y, v.w, v.h, v.ext[1], v.ext[2])
+        self._code = self._code .. string.format(DIALOG_ARC, v.ext[1], v.ext[2])
     elseif v.tp == TYPES.POLYGON then
         local names, bodies = self:_formatPoints(v.ext)
-        self._code = self._code .. string.format(DIALOG_POLYGON, v.color, v.size, bodies, names)
+        self._code = self._code .. string.format(DIALOG_POLYGON, bodies, names)
     end
 end
 
@@ -338,16 +332,16 @@ function Graphic:save(path)
 end
 
 local graphic = Graphic()
-graphic:addRectangle():setColor("#222222"):setWH(500, 500)
-graphic:addText():setXY(25, 150):setSize(48)
-graphic:addImage("./others/yellow.png", 75, 75, 350, 350):setXY(250, 50)
-graphic:addImage("./others/test.png"):setXY(25, 275):setWH(350, 350)
+graphic:setXY(0, 0):setWH(500, 500):setStyle("#222222"):addRectangle()
+graphic:setXY(250, 50):setWH(200, 200):addImage("./others/yellow.png", 75, 75, 350, 350)
+graphic:setXY(25, 275):setWH(350, 350):addImage("./others/test.png", nil, nil, nil, nil)
+graphic:setXY(25, 150):setStyle("#ff0000"):addText("Text...", 48, nil)
+-- graphic:setStyle("#2255ff"):addLine()
+-- graphic:setStyle("#2255ff"):addCurve()
+-- graphic:setStyle("#2255ff"):addBezier()
 -- graphic:addEllipse()
--- graphic:addBezier()
--- graphic:addLine()
--- graphic:addCurve()
 -- graphic:addPie()
 -- graphic:addArc()
-graphic:addPolygon()
-graphic:show()
--- graphic:save()
+graphic:setStyle("#00ff00"):addPolygon()
+-- graphic:show()
+graphic:save()
