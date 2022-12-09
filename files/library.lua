0da5bb0e92cedb183de797dbc99ebaf793cd9d61
Alimjan Yusan <kompasim@163.com>
2022-12-10 03:00:12 +0800

    Add files via upload

---
+++ files/library.lua
@@ -0,0 +1,11 @@
+--[[
+    library
+]]
+
+library = library or {}
+
+library.log30 = log30_wrapper()
+library.Stream = stream_wrapper()
+library.deflate = deflate_wrapper()
+library.pngEncode = png_encode_wrapper()
+library.pngDecode = png_decode_wrapper()
